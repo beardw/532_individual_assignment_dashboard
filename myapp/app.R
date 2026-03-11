1eba9f8d06252046881a1e4ae10e9a07948381f5
Wesley Beard <wes_beard@hotmail.com>
2026-03-11 12:06:54 -0700

    fix: update chart heights

--- myapp/app.R
+++ myapp/app.R
@@ -76,11 +76,11 @@ ui <- page_sidebar(
   layout_columns(
     card(
       card_header("Most Popular Industries"),
-      plotOutput("top_industries", height = 4000)
+      plotOutput("top_industries", height = 2000)
     ),
     card(
       card_header("Yearly Average Starting Salary (USD)"),
-      plotOutput("average_salary", height = 4000)
+      plotOutput("average_salary", height = 2000)
     ),
   ),
   card(
